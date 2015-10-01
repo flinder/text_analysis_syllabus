97d6312a8d14dc05d78db7533caea24c22808f04
flinder <fridolin.linder@gmail.com>
2015-10-01 18:35:56 -0400

    fixed parser

--- bibliography.md
+++ bibliography.md
@@ -1,22 +1,12 @@
 # Text Analysis Community Syllabus
 
-This is an automatically generated document please do not edit this document. If you want to add references insert the information into `bibliography.bib`. See the `README.md` for details
+This is an automatically generated document please do not edit it. If you want to add references, insert the information into `bibliography.bib`. See the `README.md` for details
 
-##test
+##Basics
 
-###article
-
-Rumelhart, David E and Hinton, Geoffrey E and Williams, Ronald J, (1988). Learning representations by back-propagating errors. Cognitive modeling.
-
-##no_category
-
-###article
-
-Le, Quoc V and Mikolov, Tomas, (2014). Distributed representations of sentences and documents. arXiv preprint arXiv:1405.4053.
+###book
 
 ##Neural Language Models
 
-###inproceedings
-
-Collobert, Ronan and Weston, Jason, (2008). A unified architecture for natural language processing: Deep neural networks with multitask learning. Proceedings of the 25th international conference on Machine learning.
+###article
 
--- parse_bib.py
+++ parse_bib.py
@@ -3,10 +3,10 @@ from pprint import pprint
 
 # +++++++++++++
 # CONFIG
-# +++++++++++++
 
 BIBFILE = 'bibliography.bib'
 OUTFILE = 'bibliography.md'
+# +++++++++++++
 
 # Open and parse the bib file
 with open(BIBFILE) as bibfile:
@@ -37,25 +37,32 @@ def writeline(line):
 with open(OUTFILE, 'w+') as outfile:
     
     writeline('# Text Analysis Community Syllabus')
-    writeline('This is an automatically generated document please do not edit this document. If you want to add references insert the information into `bibliography.bib`. See the `README.md` for details')
+    writeline('This is an automatically generated document please do not edit it. If you want to add references, insert the information into `bibliography.bib`. See the `README.md` for details')
     for cat in items.keys():
         writeline('##' + cat)
+
         for entry in items[cat]:
             # Collect each of the different publication types
             its = {}
             entry_type = entry['ENTRYTYPE']
             if entry_type not in its.keys():
                 its[entry_type] = []
-            its[entry_type].append(entry)
         for entry_type in its.keys():
             writeline( '###' + entry_type)
+
             for entry in its[entry_type]:
-                try:
-                    journal = entry['journal']
-                except KeyError:
-                    journal = entry['booktitle']
-                line = "{}, ({}). {}. {}.".format(entry['author'], 
-                                                  entry['year'],
-                                                  entry['title'],
-                                                  journal)
+                
+                if entry_type == 'book':
+                    line = "{} ({}). {}".format(entry['author'],
+                                                entry['year'],
+                                                entry['title'])
+                else:
+                    try:
+                        journal = entry['journal']
+                    except KeyError:
+                        journal = entry['booktitle']
+                    line = "{}, ({}). {}. {}.".format(entry['author'], 
+                                                      entry['year'],
+                                                      entry['title'],
+                                                      journal)
                 writeline(line) 
