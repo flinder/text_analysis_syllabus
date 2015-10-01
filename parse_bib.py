cfd8143ef0bd4a455549ff594dfbe5593547c00d
flinder <fridolin.linder@gmail.com>
2015-10-01 18:02:44 -0400

    added parser

---
+++ parse_bib.py
@@ -0,0 +1,61 @@
+import bibtexparser
+from pprint import pprint
+
+# +++++++++++++
+# CONFIG
+# +++++++++++++
+
+BIBFILE = 'bibliography.bib'
+OUTFILE = 'bibliography.md'
+
+# Open and parse the bib file
+with open(BIBFILE) as bibfile:
+    bibliography = bibfile.read()
+
+bib_database = bibtexparser.loads(bibliography)
+
+# Sort into categories 
+items = {}
+for entry in bib_database.entries:
+    
+    try:
+        cat = entry['category']
+    except KeyError:
+        cat = 'no_category'
+    # Make category if doesn't exist
+    if cat not in items.keys():
+        items[cat] = []
+
+    # Append entry in relevant category
+    items[cat].append(entry)
+
+def writeline(line):
+    line = line + '\n\n'
+    outfile.write(line)
+
+# Write markdown
+with open(OUTFILE, 'w+') as outfile:
+    
+    writeline('# Text Analysis Community Syllabus')
+    writeline('This is an automatically generated document please do not edit this document. If you want to add references insert the information into `bibliography.bib`. See the `README.md` for details')
+    for cat in items.keys():
+        writeline('##' + cat)
+        for entry in items[cat]:
+            # Collect each of the different publication types
+            its = {}
+            entry_type = entry['ENTRYTYPE']
+            if entry_type not in its.keys():
+                its[entry_type] = []
+            its[entry_type].append(entry)
+        for entry_type in its.keys():
+            writeline( '###' + entry_type)
+            for entry in its[entry_type]:
+                try:
+                    journal = entry['journal']
+                except KeyError:
+                    journal = entry['booktitle']
+                line = "{}, ({}). {}. {}.".format(entry['author'], 
+                                                  entry['year'],
+                                                  entry['title'],
+                                                  journal)
+                writeline(line) 
